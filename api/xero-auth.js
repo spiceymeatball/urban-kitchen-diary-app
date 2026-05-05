830ab5a4c9a3ba127efbc1592deb6bffc00768b5
spiceymeatball <info@urbankitchencafe.com.au>
2026-05-05 21:10:11 +0800

    Update Xero auth handler to create auth URL
    Refactor Xero authentication handler to build authorization URL with parameters.

--- api/xero-auth.js
+++ api/xero-auth.js
@@ -2,11 +2,16 @@ export default function handler(req, res) {
   const clientId = process.env.XERO_CLIENT_ID;
   const redirectUri = process.env.XERO_REDIRECT_URI;
   
-  res.status(200).json({
-    hasClientId: !!clientId,
-    clientIdLength: clientId ? clientId.length : 0,
-    clientIdStart: clientId ? clientId.substring(0, 4) : "empty",
-    hasRedirectUri: !!redirectUri,
-    redirectUri: redirectUri
+  const params = new URLSearchParams({
+    response_type: "code",
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    scope: "openid",
+    state: "urban-kitchen"
   });
+
+  const authUrl = "https://login.xero.com/identity/connect/authorize?" + params.toString();
+  
+  res.setHeader("Location", authUrl);
+  res.status(302).end();
 }
